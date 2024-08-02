97baf3aaf2300bb73f665df3f80abca75776d8b7
aqleemSMIT <aqleemsaylani@gmail.com>
2024-08-02 17:10:44 +0500

    add

---
+++ chpOneToTen.js
@@ -0,0 +1,87 @@
+                        //Alerts
+
+// alert( An alert is a box that pops up to give the user a message. )
+
+//  alert( "2" + "5" )
+//  console.log("Hy Pakistan")
+//  document.write("hy Pakistan")
+//  console.warn("hy Pakistan")
+                            //for print the data 
+// ...........................................................................
+                    // Variables for Strings
+
+//    let firstName = "Nasir"
+//    let secName = "Khan"
+//   console.log("student Name " + firstName + secName)   //js5
+//    console.log(`student Name $StudentName}`)  //ES6
+//.................................................................................
+                //Variables for Numbers
+
+
+    //    let age = 23;
+       
+    //    console.log("age : " + age);
+
+    // let rs = 100;   
+    // rs = 280;
+    // const price = 25
+    // price = 20
+    
+    // console.log(price);
+
+
+    // console.log("Mango :" +  rs);
+
+// ...............................................................
+                    
+            // Variable Names Legal and Illegal
+//  variable name can't contain any spaces.
+//  variable name can contain only letters, numbers, dollar signs, and underscores.
+//  variable name can't be any of JavaScript's keywords,
+//   use camelCase naming convention    (userResponseTimeLimit)                                 
+//             let avc = "khna"
+//             let  match&score = 
+//             let alert = "pak"
+
+
+// ......................................................................................
+
+                                // Math expressions Familiar operators
+            // +  ,  -  ,  *  , /  , %  
+
+ // Operator precedence  (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_precedence)
+
+    //  console.log( 2 + 9 )  //11  
+    //  console.log( 10 - 5)   // 5
+    //  console.log( 5 * 3)   //15 
+    //  console.log( 25 / 5)  // 5
+    //  console.log(20 % 2 )  //1
+    // console.log( 2+ 3 -8 * 2 / 4 +3 - 9)  // -5   -6   -7  -11 -13  +3  -1  -8  -4  12 
+
+    // console.log(2* 2* 5 -5 / 2 + 15 )  
+
+    let num1 = prompt("please inter number one") 
+    let num2 =  prompt("please inter number Two") 
+    console.log( num1 - num2)
+    
+
+    // ............................................................................
+
+    // Math expressions:  Unfamiliar operators
+
+    // num++;     num--;
+
+
+//     let rs  = 1 +1
+//     console.log(rs)
+//     let res = 5
+//     console.log("line one" +res)
+//    console.log("line Two" + --res ) 
+//     console.log( "line three" + res)
+    
+    // let num = 2
+//     console.log(num++ + num++ * num++  - --num / num-- + ++num)
+//      console.log    (  2    +  3    *   4    -   4  /   4    + 4)   
+
+// console.log(num++ + num++  * num++  - --num  * ++num)    //34  2  -6   39  5  17  10 
+// console.log(             2    +  3    *   4     -    4   *  5)
